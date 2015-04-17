6e42c1b6b0a4321fec510a336206801c661b27b9
Abby Henríquez Tejera <paradoja@gmail.com>
2015-04-17 19:15:53 +0100

    Added AR

--- Gemfile
+++ Gemfile
@@ -3,6 +3,8 @@ source 'https://rubygems.org'
 gem "aruba"
 gem "rspec"
 
+gem 'activerecord'
+gem 'sqlite3'
 gem "thor"
 
 # development
--- lib/wimdu/app.rb
+++ lib/wimdu/app.rb
@@ -1,5 +1,13 @@
 require "thor"
 
+require 'active_record'
+require 'sqlite3'
+require 'logger'
+
+ActiveRecord::Base.logger = Logger.new(File.join 'logs', 'debug.log')
+configuration = YAML::load(IO.read('config/database.yml'))
+ActiveRecord::Base.establish_connection(configuration['production'])
+
 module Wimdu
   class App < Thor
     desc "new", "Add a new property"
