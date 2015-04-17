73894239c703154d3cea10ae3e14f17a235acb55
Abby Henríquez Tejera <paradoja@gmail.com>
2015-04-17 19:37:20 +0100

    Added models

--- lib/wimdu/serializer.rb
+++ lib/wimdu/serializer.rb
@@ -2,6 +2,10 @@ require 'active_record'
 require 'sqlite3'
 require 'logger'
 
+# init code for the serialization & models
+
+Dir['./lib/wimdu/models/**/*.rb'].each(&method(:require))
+
 ActiveRecord::Base.logger = Logger.new(File.join 'logs', 'debug.log')
 configuration = YAML::load(IO.read('config/database.yml'))
-ActiveRecord::Base.establish_connection(configuration['production'])
+ActiveRecord::Base.establish_connection(ENV['WIMDU_ENV'] || configuration['production'])
